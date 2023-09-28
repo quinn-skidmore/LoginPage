8f35927d2607213aabadeaa59c2b426b600eb4f5
quinn-skidmore <qman30austin@gmail.com>
2023-09-28 10:30:15 -0500

    Initial Commit has basic screens

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class LoginPageApp(App):
+    pass
+
+
+class LoginManager(ScreenManager):
+    pass
+
+
+class Question1Screen(Screen):
+    pass
+
+
+class CorrectScreen(Screen):
+    pass
+
+
+class ErrorScreen(Screen):
+    pass
+
+
+
+LoginPageApp().run()
