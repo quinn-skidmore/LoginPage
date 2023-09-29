95cc0ea33e496b442cbd1d1aa527fbc1b11f57ce
quinn-skidmore <qman30austin@gmail.com>
2023-09-29 10:30:41 -0500

    Question 1 and 2 in and working

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-
-class LoginPageApp(App):
-    pass
+Builder.load_file("QuizPage.kv")
 
 
-class LoginManager(ScreenManager):
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
+
+
+class QuizManager(ScreenManager):
     pass
 
 
 class Question1Screen(Screen):
-    pass
+    def answer_question(self,is_correct):
+        if is_correct:
+            print("Correct")
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
+
+
+class Question2Screen(Screen):
+    def check_answer(self, answer):
+        if answer.lower() == "monkey":
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
 
 
 class CorrectScreen(Screen):
-    pass
+    def next_question(self):
+        self.manager.current = "question2"
 
 
 class ErrorScreen(Screen):
     pass
 
 
-
-LoginPageApp().run()
+QuizPageApp().run()
