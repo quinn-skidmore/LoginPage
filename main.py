aaad5f986d9c5ddf6304a7c62cda91aaeb0c2c5c
quinn-skidmore <qman30austin@gmail.com>
2023-10-03 09:56:50 -0500

    Finished Quiz

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+'''
+Builder.load_file("QuizPage.kv")
 
+class LoginPageApp(App):
+    def build(self):
+        return LoginManager()
+
+class LoginManager(ScreenManager):
+    pass
+class CreateOrSignInScreen(Screen):
+    def answer_question(self,is_correct):
+        if is_correct:
+            print("Correct")
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
+
+
+
+'''
 Builder.load_file("QuizPage.kv")
 
 
 class QuizPageApp(App):
+    question_index = 1
     def build(self):
         return QuizManager()
 
@@ -24,20 +44,30 @@ class Question1Screen(Screen):
 
 
 class Question2Screen(Screen):
+    has_attempted = False
     def check_answer(self, answer):
         if answer.lower() == "monkey":
             self.manager.current = "correct"
+        elif not self.has_attempted:
+            self.ids.animlabel.color = (1,0,0,1)
+            self.has_attempted = True
         else:
+            self.ids.animlabel.color = (1, 1, 1, 1)
             self.manager.current = "error"
+            self.has_attempted = False
+
 
 
 class CorrectScreen(Screen):
     def next_question(self):
-        self.manager.current = "question2"
+        QuizPageApp.question_index += 1
+        self.manager.current = "question" + str(QuizPageApp.question_index)
 
 
 class ErrorScreen(Screen):
-    pass
+    def retry_quiz(self):
+        self.manager.current = "question1"
+        QuizPageApp.question_index = 1
 
 
 QuizPageApp().run()
